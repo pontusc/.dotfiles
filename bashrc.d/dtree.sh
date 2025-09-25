2857b553978599fc3fa4f9ea17803214227aae17
Pontus Curtsson <pontuscurtsson@gmail.com>
2025-09-25 21:41:01 +0200

    Init branch for omarchy setup

--- bashrc.d/dtree.sh
+++
@@ -1,8 +0,0 @@
-# Runs tree with directories only and default depth 2
-function dtree(){
-    if [ -z "$1" ];then
-        tree -d -L 2
-    else
-        tree -d -L "$1"
-    fi
-}
--- install-scripts/lazydocker.sh
+++
@@ -1,1 +0,0 @@
-curl https://raw.githubusercontent.com/jesseduffield/lazydocker/master/scripts/install_update_linux.sh | bash
