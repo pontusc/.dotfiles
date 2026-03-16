2422c69d5a0a177eb9d1d5db0f535318e8d5fc2e
Pontus Curtsson <pontus.curtsson@alchemyinnovations.se>
2026-03-16 14:54:35 +0100

    Update bash

--- bash-linux/.config/bash/04-functions.bash
+++ bash-linux/.config/bash/04-functions.bash
@@ -6,3 +6,17 @@ tmux() {
         command tmux "$@"
     fi
 }
+
+kds() {
+  if [ -z "$1" ]; then
+    echo "Usage: kds <secret-name> [-n <namespace>]"
+    return 1
+  fi
+  
+  # Extracts the secret name
+  local secret_name=$1
+  # Shifts the arguments so any remaining flags (like -n argocd) are passed to kubectl
+  shift
+  
+  kubectl get secret "$secret_name" "$@" -o json | jq '.data | map_values(@base64d)'
+}
--- bash-linux/.config/bash/05-aliases.bash
+++ bash-linux/.config/bash/05-aliases.bash
@@ -19,10 +19,13 @@ alias t="talosctl"
 
 # Config files
 alias nconf="nvim -c \"cd ~/.config/nvim/\""
-alias bconf="nvim ~/.bashrc"
+alias bconf="nvim ~/.config/bash"
 alias hconf="nvim ~/.config/hypr/"
 alias tconf="nvim ~/.config/tmux/tmux.conf"
 
 # Lazy tools
 alias lgt="lazygit"
 alias ldk="lazydocker"
+
+# IP check
+alias whatsmyip="curl -s https://ifconfig.me"
