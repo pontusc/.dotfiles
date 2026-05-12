73e6c0048b74f87102c670269a0c504a8bfc5f93
Pontus Curtsson <pontus.curtsson@alchemyinnovations.se>
2026-05-12 16:53:41 +0200

    Handle idle ourselves

--- bash-work/.config/bash/06-completions.bash
+++ bash-work/.config/bash/06-completions.bash
@@ -1,6 +1,6 @@
 # Sesh completions
 if command -v sesh &> /dev/null; then
-    source <(sesh completion bash)
+  source <(sesh completion bash)
 fi
 
 # Talosctl completions
@@ -11,15 +11,19 @@ fi
 
 # Kubectl completions
 if command -v kubectl &> /dev/null; then
-    source <(kubectl completion bash)
-    complete -o default -F __start_kubectl k
+  source <(kubectl completion bash)
+  complete -o default -F __start_kubectl k
 fi
 
 # The next line enables shell command completion for gcloud.
 if [ -f '/home/pontusc/google-cloud-sdk/completion.bash.inc' ]; then . '/home/pontusc/google-cloud-sdk/completion.bash.inc'; fi
 
-
 # Terraform completions
 if command -v terraform &> /dev/null; then
   complete -o nospace -C terraform tf
+fi
+
+# Terraform completions
+if command -v netbird &> /dev/null; then
+  source <(netbird completion bash)
 fi
