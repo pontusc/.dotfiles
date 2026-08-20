f64d24ee3b47888ad9a517c7b3134ec2af86c7d9
Pontus Curtsson <pontus.curtsson@alchemyinnovations.se>
2026-08-20 18:58:44 +0200

    Cleanup openvpn conf and add ufw ssh toggle
    Cleanup openvpn conf and add ufw ssh toggle

--- bash-work/.config/bash/07-work-aliases.bash
+++ bash-work/.config/bash/07-work-aliases.bash
@@ -1,9 +1,3 @@
-# OpenVPN Shortcuts
-# OpenVPN configuration is located at /etc/openvpn/client/alchemy.conf
-alias vpn-up='sudo systemctl start openvpn-client@alchemy && echo "VPN Starting..." && sleep 2 && curl -s https://ifconfig.me && echo ""'
-alias vpn-down='sudo systemctl stop openvpn-client@alchemy && echo "VPN Stopped."'
-alias vpn-stat='systemctl status openvpn-client@alchemy'
-
 # Launch a temporary network testing pod in Kubernetes
 kube-netshell() {
   local namespace="${1:-}"
