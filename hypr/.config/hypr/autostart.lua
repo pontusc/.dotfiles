62c024873ea05bb65b4a7ee21f3108ae86a9cefe
Pontus Curtsson <pontus.curtsson@alchemyinnovations.se>
2026-09-01 14:39:42 +0200

    Fix netbird-ui on startup

--- hypr/.config/hypr/autostart.lua
+++ hypr/.config/hypr/autostart.lua
@@ -1,3 +1,3 @@
 -- Extra autostart processes.
-o.launch_on_start("env WEBKIT_DISABLE_DMABUF_RENDERER=1 netbird-ui")
+o.launch_on_start("env WEBKIT_DISABLE_DMABUF_RENDERER=1 netbird-ui --daemon-addr unix:///var/run/netbird/main.sock")
 o.launch_on_start("slack --gtk-version=3 -s --startup")
